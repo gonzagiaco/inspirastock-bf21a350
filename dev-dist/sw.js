2cf47bf4f306a1d0d84431c8a38d7d1d450b57aa
JDnasello <jnasellopron@gmail.com>
2026-01-20 11:59:07 -0300

    Fix: calculated column use current price with the principal key column

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -80,7 +80,7 @@ define(['./workbox-147b8f7b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.mvag7856auo"
+    "revision": "0.39972fso2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
